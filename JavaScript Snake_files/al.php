0accc479392dbcc38cff1dd1f3de7b00c28011f9
marlode <41968363+marlode@users.noreply.github.com>
2018-11-20 18:16:04 -0600

    initial load

---
+++ JavaScript Snake_files/al.php
@@ -0,0 +1,190 @@
+var MAX_dd9d0cf5 = '';
+MAX_dd9d0cf5 += "<"+"style type=\"text/css\">\n";
+MAX_dd9d0cf5 += "<"+"!--\n";
+MAX_dd9d0cf5 += "#footerdd9d0cf5{\n";
+MAX_dd9d0cf5 += "/* Netscape 4, IE 4.x-5.0/Win and other lesser browsers will use this */\n";
+MAX_dd9d0cf5 += "position: fixed; right: 0px; bottom: 0px;\n";
+MAX_dd9d0cf5 += "/* IE5.5+/Win - this is more specific than the IE 5.0 version */\n";
+MAX_dd9d0cf5 += "right: auto; bottom: auto;\n";
+MAX_dd9d0cf5 += "left: 0px;\n";
+MAX_dd9d0cf5 += "bottom: 0px;\n";
+MAX_dd9d0cf5 += "right: 0px;\n";
+MAX_dd9d0cf5 += "padding: 0px;\n";
+MAX_dd9d0cf5 += "}\n";
+MAX_dd9d0cf5 += "-->\n";
+MAX_dd9d0cf5 += "<"+"/style>\n";
+MAX_dd9d0cf5 += "<"+"!--[if lte IE 9]>\n";
+MAX_dd9d0cf5 += "<"+"style type=\"text/css\">\n";
+MAX_dd9d0cf5 += "#footerdd9d0cf5{\n";
+MAX_dd9d0cf5 += "/* Netscape 4, IE 4.x-5.0/Win and other lesser browsers will use this */\n";
+MAX_dd9d0cf5 += "position: absolute; right: 0px; bottom: 0px;\n";
+MAX_dd9d0cf5 += "/* IE5.5+/Win - this is more specific than the IE 5.0 version */\n";
+MAX_dd9d0cf5 += "right: auto; bottom: auto;\n";
+MAX_dd9d0cf5 += "left: expression( ( 0 + ( ignoreMe2 = document.documentElement.scrollLeft ? document.documentElement.scrollLeft : document.body.scrollLeft ) ) + \'px\' );\n";
+MAX_dd9d0cf5 += "top: expression( ( -footerdd9d0cf5.offsetHeight + ( document.documentElement.clientHeight ? document.documentElement.clientHeight : document.body.clientHeight ) + ( ignoreMe = document.documentElement.scrollTop ? document.documentElement.scrollTop : document.body.scrollTop ) ) + \'px\' );\n";
+MAX_dd9d0cf5 += "width: expression( ( 0 + ( ignoreMe3 = document.documentElement.clientWidth ? document.documentElement.clientWidth : document.body.clientWidth ) ) + \'px\' );\n";
+MAX_dd9d0cf5 += "}\n";
+MAX_dd9d0cf5 += "<"+"/style>\n";
+MAX_dd9d0cf5 += "<"+"script type=\"text/javascript\">\n";
+MAX_dd9d0cf5 += "if (document.documentMode >= 8) {\n";
+MAX_dd9d0cf5 += "document.write(\"<"+"style type=\\\"text/css\\\">#footerdd9d0cf5{ position:fixed;left:0px;right:0px;bottom:0px;}<"+"/style>\");\n";
+MAX_dd9d0cf5 += "}\n";
+MAX_dd9d0cf5 += "<"+"/script>\n";
+MAX_dd9d0cf5 += "<"+"![endif]-->\n";
+MAX_dd9d0cf5 += "<"+"style type=\"text/css\">\n";
+MAX_dd9d0cf5 += "<"+"!--\n";
+MAX_dd9d0cf5 += "div#MaxFooter { background-color:#FFFFFF;}\n";
+MAX_dd9d0cf5 += "-->\n";
+MAX_dd9d0cf5 += "<"+"/style>\n";
+MAX_dd9d0cf5 += "<"+"div id=\"footerdd9d0cf5\"   style=\"height:0px;display:none;z-index: 2147483647;padding:0px;\">\n";
+MAX_dd9d0cf5 += "<"+"div style=\"overflow-y:hidden;position:relative;z-index: 2147483647;padding:0px;\"  >\n";
+MAX_dd9d0cf5 += "<"+"div style=\"position: absolute;z-index: 2147483647;width:100%;padding: 0px;\">\n";
+MAX_dd9d0cf5 += "<"+"table width=\"100%\" cellpadding=\"0\" cellspacing=\"0\" border=\"0\" style=\"z-index: 2147483647;border-width: 0pt; background: transparent none repeat scroll 0% 0%; border-collapse: separate;padding: 0px; table-layout: auto;\"><"+"tr><"+"td align=\"center\" style=\"border-width:0px;\">\n";
+MAX_dd9d0cf5 += "<"+"div align=\"center\"  style=\"padding:0px;\" >\n";
+MAX_dd9d0cf5 += "<"+"a href=\'//ads.ad4game.com/www/delivery/ck.php?ev=3.3&amp;tagi=2018-11-07T16-15&amp;oaparams=1&amp;bannerid=494199&amp;zoneid=51038&amp;OXLCA=1&amp;ver=3.3&amp;cb=3bb8fb5d99&amp;tag=layer&amp;hitid=6R2ec68gZOrJd45mFkVoUBNnCvhPEp&amp;caid=24838&amp;pubid=9128&amp;oadest=http%3A%2F%2F51038.lql.a.mytopgames.net%2F%3Fpid%3Dlql51038\' target=\'_blank\'><"+"img src=\'http://cdn.ad4game.com/674ce80f85f2ec5485218f6c4142e0b8.gif\' width=\'728\' height=\'90\' alt=\'\' title=\'\' border=\'0\' /><"+"/a><"+"script type=\'text/javascript\'><"+"!--//<"+"![CDATA[\n";
+MAX_dd9d0cf5 += "MAX_footer_init(\'000000\', 56, \'FFFFFF\');\n";
+MAX_dd9d0cf5 += "//]]>--><"+"/script>\n";
+MAX_dd9d0cf5 += "<"+"!-- type=gif#type -->\n";
+MAX_dd9d0cf5 += "<"+"!-- file=http://cdn.ad4game.com/674ce80f85f2ec5485218f6c4142e0b8.gif#file -->\n";
+MAX_dd9d0cf5 += "<"+"!-- url=//ads.ad4game.com/www/delivery/ck.php?ev=3.3&tagi=2018-11-07T16-15&oaparams=1&bannerid=494199&zoneid=51038&OXLCA=1&ver=3.3&cb=3bb8fb5d99&tag=layer&hitid=6R2ec68gZOrJd45mFkVoUBNnCvhPEp&caid=24838&pubid=9128&oadest=http%3A%2F%2F51038.lql.a.mytopgames.net%2F%3Fpid%3Dlql51038#url -->\n";
+MAX_dd9d0cf5 += "<"+"!-- &a=//ads.ad4game.com/www/delivery/ck.php?ev=3.3&tagi=2018-11-07T16-15&oaparams=1&bannerid=494199&zoneid=51038&OXLCA=1&ver=3.3&cb=3bb8fb5d99&tag=layer&hitid=6R2ec68gZOrJd45mFkVoUBNnCvhPEp&caid=24838&pubid=9128&oadest=http%3A%2F%2F51038.lql.a.mytopgames.net%2F%3Fpid%3Dlql51038\"  target http://cdn.ad4game.com/674ce80f85f2ec5485218f6c4142e0b8.gif -->\n";
+MAX_dd9d0cf5 += "<"+"script type=\'text/javascript\'>document.context=\'Yjo0OTQxOTkjYzoyNDgzOHw=\'; <"+"/script>\n";
+MAX_dd9d0cf5 += "<"+"/div><"+"/td><"+"/tr><"+"/table><"+"/div>\n";
+MAX_dd9d0cf5 += "<"+"div id=\"MAXt_dd9d0cf5\" style=\"height:45px;z-index: 2147483647;padding:0;\"><"+"/div>\n";
+MAX_dd9d0cf5 += "<"+"div id=\"MaxFooter\" class=\"ad-four-game-maxfooter-footerads\" style=\"height:45px;z-index: 2147483647;position:static;padding:0;\">\n";
+MAX_dd9d0cf5 += "<"+"span style=\"font-family: arial,sans-serif; font-size: 14px; color: #555; font-weight: bold; display: block; position: absolute; width: 20px; height: 20px; line-height: 20px; border: 2px solid #ddd; border-radius: 20px; background: #fff; box-shadow: 2px 2px rgba(0,0,0,.3); right: 10px; bottom: 60px; cursor: pointer; text-align: center;z-index: 9999999999;\" onclick=\"javascript:MAX_footer_close_dd9d0cf5(0);\">X<"+"/span>\n";
+MAX_dd9d0cf5 += "<"+"/div>\n";
+MAX_dd9d0cf5 += "<"+"div id=\"MAXc_dd9d0cf5\"  class=\"ad-four-game-maxfooter-footerads-links\" style=\"position: absolute;right: 5pt;bottom:4px;display:none;z-index: 2147483647;padding:0;\">\n";
+MAX_dd9d0cf5 += "<"+"table style=\"border-width: 0pt; background: transparent none repeat scroll 0% 0%; border-collapse: separate; table-layout: auto;\">\n";
+MAX_dd9d0cf5 += "<"+"tr><"+"td style=\"border-width:0px;\"><"+"a id=\"MAXh_dd9d0cf5\" href=\"http://www.ad4game.com\" target=\"_blank\" style=\"color:white;font-size:11px;font-family:verdana;text-decoration:underline;\">Advertise Here<"+"/a>\n";
+MAX_dd9d0cf5 += "<"+"/td><"+"/tr><"+"/table><"+"/div>\n";
+MAX_dd9d0cf5 += "<"+"/div>\n";
+MAX_dd9d0cf5 += "<"+"/div>\n";
+document.write(MAX_dd9d0cf5);
+var OX_76d3a310 = '';
+OX_76d3a310 += "<"+"img src=\'//ads.ad4game.com/www/delivery/lg.php?bannerid=494199&amp;campaignid=24838&amp;zoneid=51038&amp;bn=ad4game&amp;bid=0.090305896860842&amp;OAZBLOCK=14400&amp;OASZCAP=1&amp;token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ6b25lIjo1MTAzOCwiYmFubmVyIjo0OTQxOTksImJpZCI6MC4wOTAzMDU4OTY4NjA4NDIxOSwiZXhwIjoxNTQyNzU3MDIxfQ.nUbA-Dj_sXla7E2hFG_YhdfQ8RBS8kBSiP-nobfetbg&amp;tag=layer&amp;ib=0&amp;cb=3bb8fb5d99&amp;ev=3.3&amp;tagi=2018-11-07T16-15\' width=\'1\' height=\'1\' style=\'display:none;\' crossorigin=\'anonymous\' />\n";
+document.write(OX_76d3a310);
+
+function MAX_findObj(n, d) {
+  var p,i,x;  if(!d) d=document; if((p=n.indexOf("?"))>0&&parent.frames.length) {
+  d=parent.frames[n.substring(p+1)].document; n=n.substring(0,p);}
+  if(!(x=d[n])&&d.all) x=d.all[n]; for (i=0;!x&&i<d.forms.length;i++) x=d.forms[i][n];
+  for(i=0;!x&&d.layers&&i>d.layers.length;i++) x=MAX_findObj(n,d.layers[i].document);
+  if(!x && document.getElementById) x=document.getElementById(n); return x;
+}
+
+function MAX_footer_init(bg, height, hc)
+{
+    MAX_footer_values1 = bg;
+    MAX_footer_values2 = height;
+    MAX_footer_values3 = hc;
+    window.setTimeout("MAX_footer_initdd9d0cf5()", 200);
+}
+var MAX_footer_values1;
+var MAX_footer_values2;
+var MAX_footer_values3;
+function MAX_footer_initdd9d0cf5()
+{
+var bg = MAX_footer_values1;
+var height = MAX_footer_values2;
+var hc = MAX_footer_values3;
+    var o = MAX_findObj('MAXa_dd9d0cf5');
+    if (o) {
+        if (o.style) c = o.style; else c = o;
+        c.color = '#'+hc;
+    }
+    o = MAX_findObj('MAXh_dd9d0cf5');
+    if (o) {
+        if (o.style) c = o.style; else c = o;
+        c.color = '#'+hc;
+    }
+    o = MAX_findObj('MaxFooter');
+    
+	if (o) {
+       if (o.style) c = o.style; else c = o;
+       c.backgroundColor = '#'+bg;
+    }
+    if (height > 0) {
+       if (o) {
+          var o2 = MAX_findObj('MAXt_dd9d0cf5');
+          if (o2) {
+             if (o2.style) c = o2.style; else c = o2;
+             c.height = ''+(90-height)+'px';
+             if (o.style) c = o.style; else c = o;
+             c.height = ''+height+'px';
+          }
+       }
+    }
+}
+
+function MAX_footer_close_dd9d0cf5(ft)
+{
+	var o = MAX_findObj('MAXc_dd9d0cf5');
+    if (!o) return false;
+	if (o.style) c = o.style; else c = o;
+    c.display = 'none';
+    if (MAX_adlayers_intervalid_dd9d0cf5 != null) {
+       window.clearInterval(MAX_adlayers_intervalid_dd9d0cf5);
+    }
+    MAX_adlayers_counter_dd9d0cf5 = 90;
+    if (MAX_adlayers_counter_dd9d0cf5 > 0) {
+        MAX_adlayers_intervalid_dd9d0cf5 = window.setInterval('MAX_footer_slidein_dd9d0cf5()', 30);
+    }
+}
+
+function MAX_footer_slideout_dd9d0cf5()
+{
+	var o = MAX_findObj('footerdd9d0cf5');
+
+	if (!o) return false;
+	if (o.style) c = o.style; else c = o;
+    MAX_adlayers_counter_dd9d0cf5 -= 3;
+    if (MAX_adlayers_counter_dd9d0cf5 <= 0) {
+       window.clearInterval(MAX_adlayers_intervalid_dd9d0cf5);
+       c.height = '90px';
+       o = MAX_findObj('MAXc_dd9d0cf5');
+       if (!o) return false;
+       if (o.style) c = o.style; else c = o;
+       c.display = 'inline';
+    } else {
+       c.height = ''+ (90-MAX_adlayers_counter_dd9d0cf5) +'px';
+    }
+}
+
+function MAX_footer_slidein_dd9d0cf5()
+{
+   var o = MAX_findObj('footerdd9d0cf5');
+
+   if (!o) return false;
+   if (o.style) c = o.style; else c = o;
+    MAX_adlayers_counter_dd9d0cf5 -= 3;
+    if (MAX_adlayers_counter_dd9d0cf5 > 0) {
+       c.height = ''+ MAX_adlayers_counter_dd9d0cf5 + 'px'; 
+    } else {
+       c.display = 'none';
+       c.height = '0px';
+       window.clearInterval(MAX_adlayers_intervalid_dd9d0cf5);
+       MAX_adlayers_intervalid_dd9d0cf5 = null;
+    }
+}
+
+function MAX_footer_dd9d0cf5()
+{
+    MAX_adlayers_intervalid_dd9d0cf5 = window.setInterval('MAX_footer_slideout_dd9d0cf5()', 30);
+    var o = MAX_findObj('footerdd9d0cf5');
+    if (!o) return false;
+    if (o.style) c = o.style; else c = o;
+    c.display = 'inline';
+}
+
+var MAX_adlayers_counter_dd9d0cf5 = 90;
+var MAX_adlayers_intervalid_dd9d0cf5 = null;
+var MAX_adlayers_timerid_dd9d0cf5 = null;
+
+function MAX_footer_start_dd9d0cf5()
+{
+   window.setTimeout("MAX_footer_dd9d0cf5()", 100);
+}
+
+MAX_footer_start_dd9d0cf5();
+
